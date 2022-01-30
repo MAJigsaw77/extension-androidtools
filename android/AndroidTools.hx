1942c5d2cedf68dfde20c396cedc9ac5388e73cc
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-01-30 18:32:54 +0200

    Ok, this should work now

--- android/AndroidTools.hx
+++ android/AndroidTools.hx
@@ -63,6 +63,12 @@ class AndroidTools {
 		return 0;
 	}
 
+	public static function getFileUrl(path:String):String {
+		#if android
+		return getFileUrl_jni(path);
+                #end
+	}
+
 	#if android
 	private static var request_permissions_jni = JNI.createStaticMethod("org.haxe.extension.AndroidTools", "requestPermissions", "([Ljava/lang/String;I)V");
 	private static var getGrantedPermissions_jni = JNI.createStaticMethod("org.haxe.extension.AndroidTools", "getGrantedPermissions", "()[Ljava/lang/String;");
@@ -71,5 +77,6 @@ class AndroidTools {
 	private static var getExternalStorageDirectory_jni = JNI.createStaticMethod("org.haxe.extension.AndroidTools", "getExternalStorageDirectory", "()Ljava/lang/String;");
 	private static var goToSettings_jni = JNI.createStaticMethod("org.haxe.extension.AndroidTools", "goToSettings", "()V");
 	private static var getSDKversion_jni = JNI.createStaticMethod("org.haxe.extension.AndroidTools", "getSDKversion", "()I");
+        private static var getFileUrl_jni = JNI.createStaticMethod("org.haxe.extension.AndroidTools", "getFileUrl", "(Ljava/lang/String;)Ljava/lang/String;");
 	#end
 }
