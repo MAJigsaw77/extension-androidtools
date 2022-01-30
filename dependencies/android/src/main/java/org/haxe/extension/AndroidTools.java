d84fe31db6ef26cf4a824b12c78a3503d48b1fcc
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-01-30 19:49:56 +0200

    Update AndroidTools.java

--- dependencies/android/src/main/java/org/haxe/extension/AndroidTools.java
+++ dependencies/android/src/main/java/org/haxe/extension/AndroidTools.java
@@ -20,6 +20,7 @@ import org.haxe.lime.HaxeObject;
 import java.util.ArrayList;
 import java.util.List;
 import java.lang.Object;
+import java.io.File;
 
 /* 
 	You can use the Android Extension class in order to hook
