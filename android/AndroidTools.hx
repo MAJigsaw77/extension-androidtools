850004a2f8f9f7e97a45ffe1dd7cfa3e1056f4d9
jigsaw-4277821 <mihaialexandru427@yahoo.com>
2022-05-03 19:23:09 +0300

    updates

--- android/AndroidTools.hx
+++ android/AndroidTools.hx
@@ -2,6 +2,8 @@ package android;
 
 #if android
 import lime.system.JNI;
+import haxe.Json;
+import haxe.Constraints.Function;
 import android.stuff.*;
 #end
 
@@ -46,4 +48,69 @@ class AndroidTools {
 		return getFileUrl_jni(path);
 	}
 	#end
+}
+
+class CallbackHelper {
+    public var listener:Map<String, Array<Function>>;
+    
+    public function new() {
+        listener = new Map();
+        listener.set(ExtensionEvent.onActivityResult, new Array());
+        listener.set(ExtensionEvent.onRequestPermissionsResult, new Array());
+
+        #if android
+        setCallback_jni(this);
+        #end
+    }
+
+    public function onActivityResult(requestCode:Int, resultCode:Int, ?data:Dynamic){
+        for (func in listener.get(ExtensionEvent.onActivityResult)){
+            func(requestCode, resultCode, new JavaObject(data));
+        }
+    }
+    public function onRequestPermissionsResult(requestCode:Int, permissions:Array<Permissions>, grantResults:Array<Int>){
+        for (func in listener.get(ExtensionEvent.onRequestPermissionsResult)){
+            func(requestCode, permissions, grantResults);
+        }
+    }
+
+    public function addEventListener(event:ExtensionEvent, func:Function){
+        listener.get(event).push(func);
+    }
+
+    public function removeEventListener(event:ExtensionEvent, func:Function){
+        listener.get(event).remove(func);
+    }
+
+    #if android
+    private static var setCallback_jni = JNI.createStaticMethod("org.haxe.extension.Tools", "setCallback", "(Lorg/haxe/lime/HaxeObject;)V");
+    #end
+}
+
+class JavaObject {
+    
+    @:noCompletion public var jobject:Dynamic;
+    public var data:Dynamic;
+
+    public function new(obj:Dynamic){
+        jobject = obj;
+        #if android
+        data = Json.parse(objectToJson_jni(obj)); // dont working for now
+        #end
+    }
+
+    public function compare(jobj:Dynamic){
+        if (Std.isOfType(jobj, JavaObject))
+            return untyped this.jobject == untyped jobj.jobject;
+        return untyped this.jobject == untyped jobj;
+    }
+
+    #if android
+    private static var objectToJson_jni = JNI.createStaticMethod("org.haxe.extension.Tools", "objectToJson", "(Ljava/lang/Object;)Ljava/lang/String;");
+    #end
+}
+
+enum abstract ExtensionEvent(String) from String to String {
+    var onActivityResult = 'onActivityResult';
+    var onRequestPermissionsResult = 'onRequestPermissionsResult';
 }
--- android/Hardware.hx
+++ android/Hardware.hx
@@ -6,26 +6,22 @@ import lime.system.JNI;
 
 class Hardware {
 	#if android
-	public static function vibrate(inputValue:Int)
-	{
+	public static function vibrate(inputValue:Int) {
 		var vibrate_jni = JNI.createStaticMethod ("org.haxe.extension.Tools", "vibrate", "(I)V");
 		vibrate_jni(inputValue);
 	}
 
-	public static function wakeUp():Void
-	{
+	public static function wakeUp():Void {
 		var wakeUp_jni = JNI.createStaticMethod ("org.haxe.extension.Tools", "wakeUp", "()V");
 		wakeUp_jni();
 	}
 
-	public static function getScreenWidth():Int
-	{
+	public static function getScreenWidth():Int {
 		var get_screen_width_jni = JNI.createStaticMethod ("org.haxe.extension.Tools", "getScreenWidth", "()I");
 		return get_screen_width_jni();
 	}
 
-	public static function getScreenHeight():Int
-	{
+	public static function getScreenHeight():Int {
 		var get_screen_height_jni = JNI.createStaticMethod ("org.haxe.extension.Tools", "getScreenHeight", "()I");
 		return get_screen_height_jni();
 	}
