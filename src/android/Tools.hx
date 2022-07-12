c2ea0f96033b417b35077127192b7e85bee4e4c9
jigsaw-4277821 <mihaialexandru427@yahoo.com>
2022-07-12 23:44:53 +0300

    Update Tools.hx

--- src/android/Tools.hx
+++ src/android/Tools.hx
@@ -17,7 +17,20 @@ class Tools
 	}
 
 	/**
+	 * Gets the value of the specified environment variable.
+	 * 
+	 * @param name the name of the environment variable
+	 */
+	public static function getenv(name:String):String
+	{
+		var getenv_jni:Dynamic = JNI.createStaticMethod("java/lang/System", "getenv", "(Ljava/lang/String;)Ljava/lang/String;");
+		return getenv_jni(name);
+	}
+
+	/**
 	 * Loads the system library specified by the libname argument.
+	 * 
+	 * @param libname the name of the library.
 	 */
 	public static function loadLibrary(libname:String):Void
 	{
