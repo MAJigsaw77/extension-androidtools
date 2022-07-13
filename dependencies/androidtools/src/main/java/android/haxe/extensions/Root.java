de8638af6241c355479ce7a18e2628a9107725c1
luckydog7 <l@d.7>
2022-07-13 18:59:03 +0300

    fix root request?

--- dependencies/androidtools/src/main/java/android/haxe/extensions/Root.java
+++ dependencies/androidtools/src/main/java/android/haxe/extensions/Root.java
@@ -1,13 +1,19 @@
 package android.haxe.extensions;
 
-import android.os.Process;
+import android.util.Log;
+
 import java.lang.Runtime;
 import org.haxe.extension.Extension;
 
 public class Root extends Extension
 {
-	public static Void requestRootPerms()
+	public static void requestRootPerms()
 	{
-		Process request = Runtime.getRuntime().exec("su");
+		try {
+			Process p = Runtime.getRuntime().exec("su");
+			p.destroy();
+		} catch (Exception e){
+			Log.e("AndroidTools", "Failed to request root permission");
+		}
 	}
 }
