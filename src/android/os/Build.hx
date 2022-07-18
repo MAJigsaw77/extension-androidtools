7207766392e0d8a5220c1c50a53e899f6320d6c0
jigsaw-4277821 <mihaialexandru427@yahoo.com>
2022-07-18 21:27:50 +0300

    Preventing a crash, maybe?

--- src/android/os/Build.hx
+++ src/android/os/Build.hx
@@ -12,47 +12,61 @@ class Build
 	/**
 	 * The name of the underlying board, like "goldfish".
 	 */
-	public static final BOARD:String = JNI.createStaticField("android/os/Build", "BOARD", "Ljava/lang/String;").get();
+	public static final BOARD:String = getStaticField("BOARD", "Ljava/lang/String;");
 
 	/**
 	 * The consumer-visible brand with which the product/hardware will be associated, if any.
 	 */
-	public static final BRAND:String = JNI.createStaticField("android/os/Build", "BRAND", "Ljava/lang/String;").get();
+	public static final BRAND:String = getStaticField("BRAND", "Ljava/lang/String;");
 
 	/**
 	 * The name of the industrial design.
 	 */
-	public static final DEVICE:String = JNI.createStaticField("android/os/Build", "DEVICE", "Ljava/lang/String;").get();
+	public static final DEVICE:String = getStaticField("DEVICE", "Ljava/lang/String;");
 
 	/**
 	 * No Description.
 	 */
-	public static final HOST:String = JNI.createStaticField("android/os/Build", "HOST", "Ljava/lang/String;").get();
+	public static final HOST:String = getStaticField("HOST", "Ljava/lang/String;");
 
 	/**
 	 * Either a changelist number, or a label like "M4-rc20".
 	 */
-	public static final ID:String = JNI.createStaticField("android/os/Build", "ID", "Ljava/lang/String;").get();
+	public static final ID:String = getStaticField("ID", "Ljava/lang/String;");
 
 	/**
 	 * The name of the overall product.
 	 */
-	public static final PRODUCT:String = JNI.createStaticField("android/os/Build", "PRODUCT", "Ljava/lang/String;").get();
+	public static final PRODUCT:String = getStaticField("PRODUCT", "Ljava/lang/String;");
 
 	/**
 	 * The manufacturer of the product/hardware.
 	 */
-	public static final MANUFACTURER:String = JNI.createStaticField("android/os/Build", "MANUFACTURER", "Ljava/lang/String;").get();
+	public static final MANUFACTURER:String = getStaticField("MANUFACTURER", "Ljava/lang/String;");
 
 	/**
 	 * The end-user-visible name for the end product.
 	 */
-	public static final MODEL:String = JNI.createStaticField("android/os/Build", "MODEL", "Ljava/lang/String;").get();
+	public static final MODEL:String = getStaticField("MODEL", "Ljava/lang/String;");
 
 	/**
 	 * No Description.
 	 */
-	public static final USER:String = JNI.createStaticField("android/os/Build", "USER", "Ljava/lang/String;").get();
+	public static final USER:String = getStaticField("USER", "Ljava/lang/String;");
+
+	private function getStaticField(memberName:String, signature:String):Dynamic
+	{
+		try
+		{
+			return JNI.createStaticField("android/os/Build", memberName, signature).get();
+		}
+		catch(e:Dynamic)
+		{
+			Application.current.window.alert("JNI Return Error: " + e, "Build from extension-androitools");
+		}
+
+		return null;
+	}
 }
 
 class VERSION
@@ -61,21 +75,29 @@ class VERSION
 	 * The internal value used by the underlying source control to represent this build.
 	 * E.g., a perforce changelist number or a git hash.
 	 */
-	public static final INCREMENTAL:String = JNI.createStaticField("android/os/Build$VERSION", "INCREMENTAL", "Ljava/lang/String;").get();
+	public static final INCREMENTAL:String = getStaticField"INCREMENTAL", "Ljava/lang/String;");
 
 	/**
 	 * The user-visible version string.
 	 */
-	public static final RELEASE:String = JNI.createStaticField("android/os/Build$VERSION", "RELEASE", "Ljava/lang/String;").get();
-
-	/**
-	 * The user-visible SDK version of the framework in its raw String representation.
-	 */
-	@:deprecated("Use SDK_INT instead")
-	public static final SDK:String = JNI.createStaticField("android/os/Build$VERSION", "SDK", "Ljava/lang/String;").get();
+	public static final RELEASE:String = getStaticField("RELEASE", "Ljava/lang/String;");
 
 	/**
 	 * The SDK version of the software currently running on this hardware device.
 	 */
-	public static final SDK_INT:Int = JNI.createStaticField("android/os/Build$VERSION", "SDK_INT", "I").get();
+	public static final SDK_INT:Int = getStaticField("SDK_INT", "I");
+
+	private function getStaticField(memberName:String, signature:String):Dynamic
+	{
+		try
+		{
+			return JNI.createStaticField("android/os/Build$VERSION", memberName, signature).get();
+		}
+		catch(e:Dynamic)
+		{
+			Application.current.window.alert("JNI Return Error: " + e, "Build.VERISON from extension-androitools");
+		}
+
+		return null;
+	}
 }
