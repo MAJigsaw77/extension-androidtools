c25c349563001f4d0e3cbf4193c94af2aca3ef78
jigsaw-4277821 <mihaialexandru427@yahoo.com>
2022-07-31 21:41:09 +0300

    things

--- android/net/Uri.hx
+++ android/net/Uri.hx
@@ -1,10 +1,10 @@
 package android.net;
 
 #if (!android && !native && macro)
-#error "extension-androidtools is not supported on your current platform"
+#error 'extension-androidtools is not supported on your current platform'
 #end
 
-#if (openfl < "4.0.0")
+#if (openfl < '4.0.0')
 import openfl.utils.JNI;
 #else
 import lime.system.JNI;
@@ -18,39 +18,39 @@ import lime.system.JNI;
 class Uri
 {
 	/**
-	 * Decodes "%"-escaped octets in the given string using the UTF-8 scheme.
-	 * Replaces invalid octets with the unicode replacement character ("\\uFFFD").
+	 * Decodes '%'-escaped octets in the given string using the UTF-8 scheme.
+	 * Replaces invalid octets with the unicode replacement character ('\\uFFFD').
 	 * 
 	 * @param s encoded string to decode
 	 */
 	public static function decode(s:String):String
 	{
-		var decode_jni:Dynamic = JNI.createStaticMethod("android/net/Uri", "decode", "(Ljava/lang/String;)Ljava/lang/String;");
+		var decode_jni:Dynamic = JNI.createStaticMethod('android/net/Uri', 'decode', '(Ljava/lang/String;)Ljava/lang/String;');
 		return decode_jni(s);
 	}
 
 	/**
-	 * Encodes characters in the given string as "%"-escaped octets using the UTF-8 scheme.
-	 * Leaves letters ("A-Z", "a-z"), numbers ("0-9"), and unreserved characters ("_-!.~"()*") intact.
+	 * Encodes characters in the given string as '%'-escaped octets using the UTF-8 scheme.
+	 * Leaves letters ('A-Z', 'a-z'), numbers ('0-9'), and unreserved characters ('_-!.~'()*') intact.
 	 * Encodes all other characters.
 	 * 
 	 * @param s string to encode
 	 */
 	public static function encode(s:String):String
 	{
-		var encode_jni:Dynamic = JNI.createStaticMethod("android/net/Uri", "encode", "(Ljava/lang/String;)Ljava/lang/String;");
+		var encode_jni:Dynamic = JNI.createStaticMethod('android/net/Uri', 'encode', '(Ljava/lang/String;)Ljava/lang/String;');
 		return encode_jni(s);
 	}
 
 	/**
-	 * Creates a Uri from a file. The URI has the form "file://". Encodes path characters with the exception of "/".
-	 * Example: "file:///tmp/android.txt".
+	 * Creates a Uri from a file. The URI has the form 'file://'. Encodes path characters with the exception of '/'.
+	 * Example: 'file:///tmp/android.txt'.
 	 *
 	 * @param path
 	 */
 	public static function fromFile(path:String):String
 	{
-		var fromFile_jni:Dynamic = JNI.createStaticMethod("org/haxe/extension/Hardware", "fromFile", "(Ljava/lang/String;)Ljava/lang/String;");
+		var fromFile_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Hardware', 'fromFile', '(Ljava/lang/String;)Ljava/lang/String;');
 		return fromFile_jni(path);
 	}
 }
