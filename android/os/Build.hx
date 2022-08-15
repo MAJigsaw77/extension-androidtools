ec4f79e629506cf8a7ef89876e815d577b2ba9c2
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-08-15 15:25:39 +0300

    Create Build.hx

--- android/os/Build.hx
+++ android/os/Build.hx
@@ -4,11 +4,7 @@ package android.os;
 #error 'extension-androidtools is not supported on your current platform'
 #end
 
-#if (openfl < '4.0.0')
-import openfl.utils.JNI;
-#else
 import lime.system.JNI;
-#end
 
 /**
  * @see https://developer.android.com/reference/android/os/Build
@@ -19,11 +15,7 @@ import lime.system.JNI;
 @:fileXml('tags="haxe,release"')
 @:noDebug
 #end
-#if (openfl < '4.0.0')
-@:access(openfl.utils.JNI)
-#else
 @:access(lime.system.JNI)
-#end
 class Build
 {
 	/**
