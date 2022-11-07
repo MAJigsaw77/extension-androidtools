0f55395b3fa6fb9541e17a61344486d431adacce
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-07 12:12:13 +0200

    I forgot about this

--- android/content/Context.hx
+++ android/content/Context.hx
@@ -35,12 +35,14 @@ class Context
 
 	public static function setWallpaper(bitmap:BitmapData):Void
 	{
+		var getBitmap_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'getBitmap', '(Ljava/lang/String;)Landroid.graphics.Bitmap;');
+		var setWallpaper_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'setWallpaper', '(Landroid.graphics.Bitmap;)V');
+
 		var bytes:ByteArray = bitmap.encode(bitmap.rect, new PNGEncoderOptions());
 		bytes.position = 0;
 
-		var getBitmap_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'getBitmap', '(Ljava/lang/String;)Landroid.graphics.Bitmap;');
-		var setWallpaper_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'setWallpaper', '(Landroid.graphics.Bitmap;)V');
 		setWallpaper_jni(getBitmap(Base64.encode(bytes)));
+		bytes.clear();
 	}
 
 	public static function getWallpaper():BitmapData
