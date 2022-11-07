0a5f08bf8fdbdf692b7208679481baa505ddf267
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-07 10:01:04 +0200

    Da Wallpaper shits

--- android/content/Context.hx
+++ android/content/Context.hx
@@ -3,6 +3,10 @@ package android.content;
 #if (!android && !native && macro)
 #error 'extension-androidtools is not supported on your current platform'
 #end
+import openfl.display.BitmapData;
+import openfl.display.PNGEncoderOptions;
+import openfl.utils.ByteArray;
+import haxe.crypto.Base64;
 import lime.system.JNI;
 
 /**
@@ -27,5 +31,22 @@ class Context
 		var getExternalFilesDir_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'getExternalFilesDir', '(Ljava/lang/String;)Ljava/io/File;');
 		var getAbsolutePath_jni:Dynamic = JNI.createMemberMethod('java/io/File', 'getAbsolutePath', '()Ljava/lang/String;');
 		return getAbsolutePath_jni(getExternalFilesDir_jni(type));
+	}
+
+	public static function setWallpaper(bitmap:BitmapData):Void
+	{
+		var bytes:ByteArray = bitmap.encode(bitmap.rect, new PNGEncoderOptions());
+		bytes.position = 0;
+
+		var getBitmap_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'getBitmap', '(Ljava/lang/String;)Landroid.graphics.Bitmap;');
+		var setWallpaper_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'setWallpaper', '(Landroid.graphics.Bitmap;)V');
+		setWallpaper_jni(getBitmap(Base64.encode(bytes)));
+	}
+
+	public static function getWallpaper():BitmapData
+	{
+		var getBitmapToBase64_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'getBitmapToBase64', '(Landroid.graphics.Bitmap;)Ljava/lang/String;');
+		var getWallpaper_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'getWallpaper', '()Landroid.graphics.Bitmap;');
+		return BitmapData.fromBase64(getBitmapToBase64(getWallpaper()));
 	}
 }
