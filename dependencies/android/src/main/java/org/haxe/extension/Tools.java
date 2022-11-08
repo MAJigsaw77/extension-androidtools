4066a605b08139c1d25b81b7929e0782aa76a667
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-08 11:56:01 +0200

    No longer needed xd

--- dependencies/android/src/main/java/org/haxe/extension/Tools.java
+++ dependencies/android/src/main/java/org/haxe/extension/Tools.java
@@ -30,27 +30,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tools extends Extension {
-    public static String[] getGrantedPermissions() {
-        List<String> granted = new ArrayList<String>();
-
-        try {
-            PackageInfo packInfo = Extension.mainContext.getPackageManager().getPackageInfo(
-                Extension.mainContext.getPackageName(), PackageManager.GET_PERMISSIONS);
-            for (int i = 0; i < packInfo.requestedPermissions.length; i++) {
-                if ((packInfo.requestedPermissionsFlags[i] & PackageInfo.REQUESTED_PERMISSION_GRANTED) != 0) granted.add(packInfo.requestedPermissions[i]);
-            }
-        } catch (Exception e) {
-            Log.e("Tools", e.toString());
-        }
-
-        return granted.toArray(new String[granted.size()]);
-    }
-
-    public static void requestPermissions(
-        String[] permissions, int requestCode) {
-        Extension.mainActivity.requestPermissions(permissions, requestCode);
-    }
-
     public static Drawable getDrawable(String bytes) {
         byte[] bytesArray = Base64.decode(bytes, Base64.DEFAULT);
 
