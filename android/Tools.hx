8f6e5636f83b85e4abb2e406331f6cd7da2455a3
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-11 19:38:40 +0200

    Update Tools.hx

--- android/Tools.hx
+++ android/Tools.hx
@@ -16,12 +16,32 @@ import lime.system.JNI;
 class Tools
 {
 	/**
+	 * Shares a text.
+	 */
+	public static function sendText(text:String, textType:String):Void
+	{
+		var sendText_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'sendText', '(Ljava/lang/String;Ljava/lang/String;)V');
+		sendText_jni(text, textType);
+	}
+
+	/**
+	 * Launches a app.
+	 */
+	public static function launchApp(packageName:String):Void
+	{
+		var launchApp_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'launchApp', '(Ljava/lang/String;)V');
+		launchApp_jni(packageName);
+	}
+
+	/**
 	 * Sets the phone brightness, max is 1 and min is 0.
 	 */
 	public static function setBrightness(brightness:Float):Void
 	{
-		if (brightness > 1 && brightness < 0)
-			return;
+		if (brightness > 1)
+			brightness = 1;
+		else if (brightness < 0)
+			brightness = 0;
 
 		var setbrightness_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'setBrightness', '(F)V');
 		setbrightness_jni(brightness);
