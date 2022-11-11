529fa5c93aabad3bc6263a392c0424f11c2400bc
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-11 18:15:58 +0200

    Update Toast.hx

--- android/widget/Toast.hx
+++ android/widget/Toast.hx
@@ -20,68 +20,12 @@ class Toast
 	public static final LENGTH_SHORT:Int = 0;
 	public static final LENGTH_LONG:Int = 1;
 
-	private static var constructor:Dynamic;
-
-	public function new(?newCostructor:Dynamic):Void
+	/**
+	 * Makes a toast text.
+	 */
+	public static function makeText(text:String, duration:Int):Void
 	{
-		if (newCostructor != null)
-		{
-			constructor = newCostructor;
-			return;
-		}
-
-		var constructor_jni:Dynamic = JNI.createStaticMethod('android/widget/Toast', '<init>', '(Landroid/content/Context;)V');
-		var mainContext:Dynamic = JNI.createStaticField('org/haxe/extension/Extension', 'mainContext', 'Landroid/content/Context;').get();
-		constructor = constructor_jni(mainContext);
-	}
-
-	public function getDuration():Int
-		return JNI.callMember(JNI.createMemberMethod('android/widget/Toast', 'getDuration', '()I'), constructor, []);
-
-	public function getGravity():Int
-		return JNI.callMember(JNI.createMemberMethod('android/widget/Toast', 'getGravity', '()I'), constructor, []);
-
-	public function getHorizontalMargin():Float
-		return JNI.callMember(JNI.createMemberMethod('android/widget/Toast', 'getHorizontalMargin', '()F'), constructor, []);
-
-	public function getVerticalMargin():Float
-		return JNI.callMember(JNI.createMemberMethod('android/widget/Toast', 'getVerticalMargin', '()F'), constructor, []);
-
-	public function getXOffset():Int
-		return JNI.callMember(JNI.createMemberMethod('android/widget/Toast', 'getXOffset', '()I'), constructor, []);
-
-	public function getYOffset():Int
-		return JNI.callMember(JNI.createMemberMethod('android/widget/Toast', 'getYOffset', '()I'), constructor, []);
-
-	public static function makeText(text:String, duration:Int):Toast
-	{
-		var makeText_jni:Dynamic = JNI.createStaticMethod('android/widget/Toast', 'makeText', '(Landroid/content/Context;Ljava/lang/CharSequence;I)Landroid/widget/Toast;');
-		var mainContext:Dynamic = JNI.createStaticField('org/haxe/extension/Extension', 'mainContext', 'Landroid/content/Context;').get();
-
-		return new Toast(makeText_jni(mainContext, text, duration)); // i could use `this` but apparently it isn't working on static function.
-	}
-
-	public function setDuration(duration:Int):Void
-		JNI.callMember(JNI.createMemberMethod('android/widget/Toast', 'setDuration', '(I)V'), constructor, [duration]);
-
-	public function setGravity(gravity:Int, xOffset:Int, yOffset:Int):Void
-		JNI.callMember(JNI.createMemberMethod('android/widget/Toast', 'setGravity', '(III)V'), constructor, [gravity, xOffset, yOffset]);
-
-	public function setMargin(horizontalMargin:Float, verticalMargin:Float):Void
-		JNI.callMember(JNI.createMemberMethod('android/widget/Toast', 'setMargin', '(FF)V'), constructor, [horizontalMargin, verticalMargin]);
-
-	public function setText(s:String):Void
-		JNI.callMember(JNI.createMemberMethod('android/widget/Toast', 'setText', '(Ljava/lang/CharSequence;)V'), constructor, [s]);
-
-	public function show():Void
-	{
-		var show_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'showToast', '(Landroid/widget/Toast;)V');
-		show_jni(constructor);
-	}
-
-	public function cancel():Void
-	{
-		var cancel_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'cancelToast', '(Landroid/widget/Toast;)V');
-		cancel_jni(constructor);
+		var toast_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'toast', '(Ljava/lang/String;I)V');
+		toast_jni(text, duration);
 	}
 }
