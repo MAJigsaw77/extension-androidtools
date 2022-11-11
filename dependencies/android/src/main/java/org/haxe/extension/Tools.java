edc2608b6459f49f7cffd662476dd5b87f38ae5e
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-11 19:39:01 +0200

    Update Tools.java

--- dependencies/android/src/main/java/org/haxe/extension/Tools.java
+++ dependencies/android/src/main/java/org/haxe/extension/Tools.java
@@ -29,6 +29,33 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+/* 
+	You can use the Android Extension class in order to hook
+	into the Android activity lifecycle. This is not required
+	for standard Java code, this is designed for when you need
+	deeper integration.
+
+	You can access additional references from the Extension class,
+	depending on your needs:
+
+	- Extension.assetManager (android.content.res.AssetManager)
+	- Extension.callbackHandler (android.os.Handler)
+	- Extension.mainActivity (android.app.Activity)
+	- Extension.mainContext (android.content.Context)
+	- Extension.mainView (android.view.View)
+
+	You can also make references to static or instance methods
+	and properties on Java classes. These classes can be included 
+	as single files using < java path="to/File.java" /> within your
+	project, or use the full Android Library Project format (such
+	as this example) in order to include your own AndroidManifest
+	data, additional dependencies, etc.
+
+	These are also optional, though this example shows a static
+	function for performing a single task, like returning a value
+	back to Haxe from Java.
+*/
+
 public class Tools extends Extension {
 
 	public static String[] getGrantedPermissions() {
@@ -80,6 +107,7 @@ public class Tools extends Extension {
 	public static void makeText(final String message, final int duration) {
 		Extension.mainActivity.runOnUiThread(new Runnable() {
 			@Override
+
 			public void run() {
 				Toast.makeText(Extension.mainContext, message, duration).show();
 			}
@@ -96,14 +124,6 @@ public class Tools extends Extension {
 
 	public static void launchApp(final String packageName) {
 		Extension.mainActivity.startActivity(Extension.mainActivity.getPackageManager().getLaunchIntentForPackage(packageName));
-	}
-
-	public static void runIntent(final String action, final String uri) {
-		Intent intent = new Intent(action);
-
-		if (uri != null)
-			intent.setData(Uri.parse(uri));
-		Extension.mainActivity.startActivity(intent);
 	}
 
 	public static void setBrightness(float brightness) {
@@ -128,5 +148,69 @@ public class Tools extends Extension {
 
 			vibrator.vibrate(pattern, -1);
 		}
+	}
+
+	/**
+	 * Called when an activity you launched exits, giving you the requestCode 
+	 * you started it with, the resultCode it returned, and any additional data 
+	 * from it.
+	 */
+	public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+		return true;
+	}
+
+	/**
+	 * Called when the activity is starting.
+	 */
+	public void onCreate(Bundle savedInstanceState) {
+
+	}
+
+	/**
+	 * Perform any final cleanup before an activity is destroyed.
+	 */
+	public void onDestroy() {
+
+	}
+
+	/**
+	 * Called as part of the activity lifecycle when an activity is going into
+	 * the background, but has not (yet) been killed.
+	 */
+	public void onPause() {
+
+	}
+
+	/**
+	 * Called after {@link #onStop} when the current activity is being 
+	 * re-displayed to the user (the user has navigated back to it).
+	 */
+	public void onRestart() {
+
+	}
+
+	/**
+	 * Called after {@link #onRestart}, or {@link #onPause}, for your activity 
+	 * to start interacting with the user.
+	 */
+	public void onResume() {
+
+	}
+
+	/**
+	 * Called after {@link #onCreate} &mdash; or after {@link #onRestart} when  
+	 * the activity had been stopped, but is now again being displayed to the 
+	 * user.
+	 */
+	public void onStart() {
+
+	}
+
+	/**
+	 * Called when the activity is no longer visible to the user, because 
+	 * another activity has been resumed and is covering this one. 
+	 */
+	public void onStop() {
+
 	}
 }
