a0192cd9e7b14d14ceae7ba61f21a1b14b13d1c1
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-14 21:17:53 +0200

    Jdkdbdjj

---
+++ android/CallBack.hx
@@ -0,0 +1,99 @@
+package android;
+
+#if (!android && !native && macro)
+#error 'extension-androidtools is not supported on your current platform'
+#end
+import lime.system.JNI;
+import openfl.events.Event;
+import openfl.events.EventDispatcher;
+import haxe.Json;
+import haxe.Timer;
+
+/**
+ * @author Mihai Alexandru (M.A. Jigsaw)
+ */
+#if !debug
+@:fileXml('tags="haxe,release"')
+@:noDebug
+#end
+@:access(lime.system.JNI)
+class CallBack
+{
+	private static var dispatcher:EventDispatcher = new EventDispatcher();
+	private static var cleanupJobs:Array<Dynamic> = [];
+	private static var initialized:Bool = false;
+
+	public static function init():Void
+	{
+		if (initialized)
+			return;
+
+		var initCallBack_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'initCallBack"/', '(Lorg/haxe/lime/HaxeObject;)V');
+		initCallBack_jni(new CallBackHandler());
+
+		initialized = true;
+	}
+
+	public static function cleanup():Void
+	{		
+		for (job in cleanupJobs)
+			Reflect.callMethod(null, job, []);
+
+		cleanupJobs = [];
+		initialized = false;
+	}
+
+	public static function addEventListener(type:String, listener:Dynamic, useCapture:Bool = false, priority:Int = 0, useWeakReference:Bool = false):Void
+	{
+		dispatcher.addEventListener(type, listener, useCapture, priority, useWeakReference);
+		cleanupJobs.push(dispatcher.removeEventListener.bind(type, listener, useCapture));
+	}
+
+	public static function removeEventListener(type:String, listener:Dynamic, capture:Bool = false):Void
+		dispatcher.removeEventListener(type, listener, capture);
+
+	public static function dispatchEvent(event:Event):Bool {
+		Timer.delay(function()
+		{
+			dispatcher.dispatchEvent(event);
+		}, 0);
+
+		return true;
+	}
+
+	public static function hasEventListener(type:String):Bool
+		return dispatcher.hasEventListener (type);
+}
+
+class CallBackEvent extends Event
+{
+	public static inline var ACTIVITY_RESULT:String = 'activity_result';
+	public static inline var REQUEST_PERMISSIONS_RESULT:String = 'request_permissions_result';
+
+	public var content:Dynamic;
+
+	public function new(type:String, content:Dynamic)
+	{
+		super(type);
+
+		this.content = content;
+	}
+}
+
+private class CallBackHandler
+{
+	public function new() {}
+
+	public function onActivityResult(content:String):Void
+	{
+		var daEvent:CallBackEvent = new CallBackEvent(CallBackEvent.PURCHASE_CONSUME_SUCCESS, Json.parse(content));		
+		CallBack.dispatchEvent(daEvent);
+	}
+
+	public function onRequestPermissionsResult(content:String):Void
+	{
+		var daEvent:CallBackEvent = new CallBackEvent(CallBackEvent.PURCHASE_CONSUME_SUCCESS, Json.parse(content));		
+		CallBack.dispatchEvent(daEvent);
+	}
+}
+
