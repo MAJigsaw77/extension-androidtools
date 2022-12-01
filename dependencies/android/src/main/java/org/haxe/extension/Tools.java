fb8d4ceed202764b761828b8e156178f158f79ea
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-01 08:26:17 +0200

    Update Tools.java

--- dependencies/android/src/main/java/org/haxe/extension/Tools.java
+++ dependencies/android/src/main/java/org/haxe/extension/Tools.java
@@ -1,7 +1,5 @@
 package org.haxe.extension;
 
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -13,6 +11,8 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.io.File;
 import java.io.IOException;
