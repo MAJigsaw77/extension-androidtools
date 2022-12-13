edd79b98331b243bf47cccd4b7876b89e9bf0cb7
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-13 07:37:23 +0200

    Update Toast.hx

--- android/widget/Toast.hx
+++ android/widget/Toast.hx
@@ -18,9 +18,9 @@ class Toast
 	/**
 	 * Makes a toast text.
 	 */
-	public static function makeText(text:String, duration:Int):Void
+	public static function makeText(text:String, duration:Int, ?gravity:Int = 0, ?xOffset:Int = 0, ?yOffset:Int = 0):Void
 	{
-		var makeText_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'makeText', '(Ljava/lang/String;I)V');
-		makeText_jni(text, duration);
+		var makeText_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'makeToastText', '(Ljava/lang/String;IIII)V');
+		makeText_jni(text, duration, gravity, xOffset, yOffset);
 	}
 }
