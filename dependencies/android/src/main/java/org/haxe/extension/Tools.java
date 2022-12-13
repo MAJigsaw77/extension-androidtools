0f9eb4931cf23de3b1ff51bd672cd08d25efc363
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-13 10:40:43 +0200

    Oops

--- dependencies/android/src/main/java/org/haxe/extension/Tools.java
+++ dependencies/android/src/main/java/org/haxe/extension/Tools.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
