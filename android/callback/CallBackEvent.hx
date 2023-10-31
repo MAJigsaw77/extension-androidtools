56a7dd53443691163845b3475dccf377d1f20a1a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-10-31 13:15:47 +0200

    Update CallBackEvent.hx

--- android/callback/CallBackEvent.hx
+++ android/callback/CallBackEvent.hx
@@ -4,12 +4,12 @@ import openfl.events.Event;
 
 class CallBackEvent extends Event
 {
-	public static inline var ACTIVITY_RESULT:String = 'activity_result';
-	public static inline var REQUEST_PERMISSIONS_RESULT:String = 'request_permissions_result';
+	public static inline final ACTIVITY_RESULT:String = 'activity_result';
+	public static inline final REQUEST_PERMISSIONS_RESULT:String = 'request_permissions_result';
 
 	public var content:Dynamic;
 
-	public function new(type:String, content:Dynamic)
+	public function new(type:String, content:Dynamic):Void
 	{
 		super(type);
 
