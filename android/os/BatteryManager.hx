9ef9afef06e51e8e2aad9ff057e2845cb03ab270
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-01-21 21:39:22 +0200

    Added `BatteryManager` class.

---
+++ android/os/BatteryManager.hx
@@ -0,0 +1,32 @@
+package android.os;
+
+#if (!android && !native && macro)
+#error 'extension-androidtools is not supported on your current platform'
+#end
+import lime.system.JNI;
+
+class BatteryManager
+{
+	public static final BATTERY_PROPERTY_CHARGE_COUNTER:Int = 1;
+	public static final BATTERY_PROPERTY_CURRENT_NOW:Int = 2;
+	public static final BATTERY_PROPERTY_CURRENT_AVERAGE:Int = 3;
+	public static final BATTERY_PROPERTY_CAPACITY:Int = 4;
+	public static final BATTERY_PROPERTY_ENERGY_COUNTER:Int = 5;
+
+	@:noCompletion private var constructor:Dynamic;
+
+	public function new():Void
+	{
+		constructor = JNI.createStaticMethod('android/os/BatteryManager', '<init>', '()V');
+	}
+
+	public function isCharging():Bool
+	{
+		return JNI.callMember(JNI.createMemberMethod('android/os/BatteryManager', 'isCharging', '()Z'), constructor, []);
+	}
+
+	public function getProperty(id:Int):Int
+	{
+		return JNI.callMember(JNI.createMemberMethod('android/os/BatteryManager', 'getIntProperty', '(I)I'), constructor, [id]);
+	}
+}
