2045d17d71f0d1aae46824f4cefd3c44c478d611
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-01-31 16:23:25 +0200

    Testing

--- project/androidtools/src/main/java/org/haxe/extension/Tools.java
+++ project/androidtools/src/main/java/org/haxe/extension/Tools.java
@@ -1,5 +1,8 @@
 package org.haxe.extension;
 
+import android.app.Notification.Builder;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
@@ -7,6 +10,7 @@ import android.content.pm.PackageManager;
 import android.media.MediaCodecList;
 import android.media.MediaFormat;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.util.ArrayMap;
 import android.util.Log;
 import android.view.WindowManager;
@@ -201,6 +205,36 @@ public class Tools extends Extension
 		return mainContext.getFilesDir();
 	}
 
+	public static void showNotification(final String title, final String message, final String channelID, final String channelName, final int ID)
+	{
+		mainActivity.runOnUiThread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+					{
+						NotificationChannel channel = new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+						notificationManager.createNotificationChannel(channel);
+					}
+
+					Builder builder = new Builder(context, CHANNEL_ID);
+					builder.setContentTitle(title);
+					builder.setContentText(message);
+					notificationManager.notify(ID, (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN > 0) ? builder.getNotification() : builder.build());
+				}
+				catch (Exception e)
+				{
+					Log.e(LOG_TAG, e.toString());
+				}
+			}
+		});
+	}
+
 	public static File getExternalFilesDir(final String type)
 	{
 		return mainContext.getExternalFilesDir(type);
@@ -223,7 +257,7 @@ public class Tools extends Extension
 
 	public static BatteryManager getBatteryManager()
 	{
-		return (BatteryManager) Extension.mainContext.getSystemService(Context.BATTERY_SERVICE);
+		return (BatteryManager) mainContext.getSystemService(Context.BATTERY_SERVICE);
 	}
 
 	public static void initCallBack(HaxeObject hobject)
