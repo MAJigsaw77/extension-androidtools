33315764cc36405cad3c653b58da331857c18a4e
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-02-05 18:29:54 +0200

    Create Settings.hx

---
+++ android/Settings.hx
@@ -0,0 +1,20 @@
+package android;
+
+#if (!android && !native && macro)
+#error 'extension-androidtools is not supported on your current platform'
+#end
+import lime.system.JNI;
+
+class Settings
+{
+	/** 
+	 * @param setting the setting.
+	 * @param requestCode the code that should be requested.
+	 */
+	public static inline function requestPermission(setting:String, requestCode:Int = 1):Void
+	{
+		requestSetting_jni(setting, requestCode);
+	}
+
+	@:noCompletion private static var requestSetting_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'requestSetting', '(Ljava/lang/String;I)V');
+}
