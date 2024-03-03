d99caa235e5e7db7e6bd5da2ebde4874ecd6ebba
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-03-03 21:28:54 +0200

    Added these back

--- android/Tools.hx
+++ android/Tools.hx
@@ -70,6 +70,54 @@ class Tools
 		showNotification_jni(title, message, channelID, channelName, ID);
 	}
 
+	/**
+	 * Sets Activity's Title by the `title`.
+	 */
+	public static inline function setActivityTitle(title:String):Bool
+	{
+		return setActivityTitle_jni(title);
+	}
+
+	/**
+	 * Minimizes app's window.
+	 */
+	public static inline function minimizeWindow():Void
+	{
+		minimizeWindow_jni();
+	}
+
+	/**
+	 * @returns whether the device is running Android TV.
+	 */
+	public static inline function isAndroidTV():Bool
+	{
+		return isAndroidTV_jni();
+	}
+
+	/**
+	 * @returns whether the device is a Tablet.
+	 */
+	public static inline function isTablet():Bool
+	{
+		return isTablet_jni();
+	}
+
+	/**
+	 * @returns whether the device is a ChromeBook.
+	 */
+	public static inline function isChromeBook():Bool
+	{
+		return isChromeBook_jni();
+	}
+
+	/**
+	 * @returns whether the device is running in Dex Mode.
+	 */
+	public static inline function isDeXMode():Bool
+	{
+		return isDexMode_jni();
+	}
+
 	@:noCompletion private static var enableAppSecure_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'enableAppSecure', '()V');
 	@:noCompletion private static var disableAppSecure_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'disableAppSecure', '()V');
 	@:noCompletion private static var launchPackage_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'launchPackage', '(Ljava/lang/String;I)V');
@@ -77,6 +125,12 @@ class Tools
 	@:noCompletion private static var isRooted_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'isRooted', '()Z');
 	@:noCompletion private static var isDolbyAtmos_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'isDolbyAtmos', '()Z');
 	@:noCompletion private static var showNotification_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'showNotification', '(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;I)V');
+	@:noCompletion private static var setActivityTitle_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'setActivityTitle', '(Ljava/lang/String;)Z');
+	@:noCompletion private static var minimizeWindow_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'minimizeWindow', '()V');
+	@:noCompletion private static var isAndroidTV_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'isAndroidTV', '()Z');
+	@:noCompletion private static var isTablet_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'isTablet', '()Z');
+	@:noCompletion private static var isChromeBook_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'isChromeBook', '()Z');
+	@:noCompletion private static var isDexMode_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'isDeXMode', '()Z');
 }
 
 @:noCompletion private typedef ButtonData = {
